9a1df65dcb331b4beb22e798ca4761dba5b7fecf
billpcs <bpanagio@protonmail.com>
2020-03-21 20:13:39 +0200

    add tests for multiple vlans

--- tests/main.sh
+++ tests/main.sh
@@ -41,6 +41,26 @@ tcpdump="tcpdump -tqenr '$pcapname' 2>&1 | grep length"
   [ "$tcp" = "$wanted" ]
 }
 
+@test "test two vlans" {
+  $("$binary" -e 0x88a8 -v 10 -e 8100 -v 20 -l 300)
+  tcp=$(eval "$tcpdump")
+  wanted="08:00:27:2a:09:13 > 08:00:27:4c:27:11, 802.1Q-QinQ, length 300: vlan 10, p 0, ethertype 802.1Q, vlan 20, p 0, ethertype IPv4, IP0 "
+  [ "$tcp" = "$wanted" ]
+}
+
+@test "test many vlans 1" {
+  $("$binary" -e 0x88a8 -v10 -v20 -v30 -p1 -p2)
+  tcp=$(eval "$tcpdump")
+  wanted="08:00:27:2a:09:13 > 08:00:27:4c:27:11, 802.1Q-QinQ, length 666: vlan 10, p 0, ethertype 802.1Q, vlan 20, p 0, ethertype 802.1Q, vlan 30, p 1, ethertype 802.1Q, vlan 100, p 2, ethertype IPv4, IP0 "
+  [ "$tcp" = "$wanted" ]
+}
+
+@test "test many vlans 2" {
+  $("$binary" -v10 -v20 -v30 -v40 -v50 -l 120)
+  tcp=$(eval "$tcpdump")
+  wanted="08:00:27:2a:09:13 > 08:00:27:4c:27:11, 802.1Q, length 120: vlan 10, p 0, ethertype 802.1Q, vlan 20, p 0, ethertype 802.1Q, vlan 30, p 0, ethertype 802.1Q, vlan 40, p 0, ethertype 802.1Q, vlan 50, p 0, ethertype IPv4, IP0 "
+  [ "$tcp" = "$wanted" ]
+}
 
 @test "vlans greater than 4095 wrap around" {
   
